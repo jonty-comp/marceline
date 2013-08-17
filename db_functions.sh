da4276f73f2c010c3bee3a3738d0de329dfbd0d6
jonty-comp <jontysewell@gmail.com>
2013-08-17 15:25:39 +0100

    Simplified script execution

--- db_functions.sh
+++ db_functions.sh
@@ -7,6 +7,6 @@ if [ "$action" == "next" ]
 then
 	for md5 in "`$psql "select v_audio.id, md5, title, artist, path from v_audio join sustschedule on v_audio.id = sustschedule.audioid order by sustschedule.id ASC LIMIT 1;"`"
 	do
-		echo $md5
+		echo -n "$md5"
 	done
 fi
