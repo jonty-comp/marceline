7fc082abbd615682c32f11f8387c58af2d44bd96
jonty-comp <jontysewell@gmail.com>
2013-08-17 13:23:40 +0100

    Preparing for future changes

--- db_functions.sh
+++ db_functions.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+psql="psql -h dps0 -U digiplay_user -d digiplay -qAt -c "
 
-for md5 in `psql -h dps0 -U digiplay_user -d digiplay -qAt -c "select md5 from audio join sustschedule on audio.id = sustschedule.audioid order by sustschedule.id ASC LIMIT 1;"`
-do
-	echo "/mnt/dps0-0/audio/${md5:0:1}/$md5.flac"
-done
+action=$1
+
+if [ "$action" == "next" ]
+then
+	for md5 in `$psql "select md5 from audio join sustschedule on audio.id = sustschedule.audioid order by sustschedule.id ASC LIMIT 1;"`
+	do
+		echo "/mnt/dps0-0/audio/${md5:0:1}/$md5.flac"
+	done
+fi
