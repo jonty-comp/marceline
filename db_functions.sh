1aaf37b0495c69d2a0a9917055294130f05ea10f
jonty-comp <jontysewell@gmail.com>
2013-08-17 14:58:27 +0100

    Move database parameters to main configuration, process result string in liquidsoap

--- db_functions.sh
+++ db_functions.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-psql="psql -h dps0 -U digiplay_user -d digiplay -qAt -c "
+psql="psql -h $2 -p $3 -U $4 -d $5 -qAt -c "
 
 action=$1
 
 if [ "$action" == "next" ]
 then
-	for md5 in `$psql "select md5 from audio join sustschedule on audio.id = sustschedule.audioid order by sustschedule.id ASC LIMIT 1;"`
+	for md5 in "`$psql "select v_audio.id, md5, title, artist, path from v_audio join sustschedule on v_audio.id = sustschedule.audioid order by sustschedule.id ASC LIMIT 1;"`"
 	do
-		echo "/mnt/dps0-0/audio/${md5:0:1}/$md5.flac"
+		echo $md5
 	done
 fi
