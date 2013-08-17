201a30bf8ee807b2d3981c51d64e4ac386f447c1
jonty-comp <jontysewell@gmail.com>
2013-08-17 16:29:13 +0100

    Create on_next for new tracks; log artist/title from database

--- db_functions.sh
+++ db_functions.sh
@@ -5,7 +5,7 @@ action=$1
 
 if [ "$action" == "next" ]
 then
-	for md5 in "`$psql "select v_audio.id, md5, title, artist, path from v_audio join sustschedule on v_audio.id = sustschedule.audioid order by sustschedule.id ASC LIMIT 1;"`"
+	for md5 in "`$psql "select sustschedule.id, v_audio.id, md5, title, artist, path from v_audio join sustschedule on v_audio.id = sustschedule.audioid order by sustschedule.id ASC LIMIT 1;"`"
 	do
 		echo -n "$md5"
 	done
